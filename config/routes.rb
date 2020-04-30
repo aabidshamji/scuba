b331e2756186650a86cb943cd6e4d539d0b07b7d
Aabid Shamji <aabidshamji@gmail.com>
2020-04-29 21:48:48 -0500

    added about as root page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
-  root "posts#index"
+  #root "posts#index"
+
+  root "pages#about"
 
   get '/about', to: 'pages#about'
 
