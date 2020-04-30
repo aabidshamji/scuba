7280ac803dc0198c3f6a0e5d1f4b8df5aa5a761f
Aabid Shamji <aabidshamji@gmail.com>
2020-04-29 21:53:10 -0500

    revet back

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
-  #root "posts#index"
-
-  root "pages#about"
+  root "posts#index"
 
   get '/about', to: 'pages#about'
 
